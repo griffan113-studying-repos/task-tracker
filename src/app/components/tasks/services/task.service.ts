ff9ac278ae555df7e622970945d2b5d854834fbc
Eliel <elielmichel@gmail.com>
2021-09-25 15:30:01 -0300

    chore: create task service

--- src/app/components/tasks/services/task.service.ts
+++ src/app/components/tasks/services/task.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/types/Task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   constructor() {}
+
+  public async getTasks(): Promise<Task[]> {
+    // const tasks = of(TASKS);
+
+    const tasks = TASKS;
+
+    return tasks;
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from 'src/app/types/Task';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -9,9 +9,16 @@ import { TASKS } from 'src/app/mock-tasks';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  public tasks: Task[] = TASKS;
+  public tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private readonly tasksService: TaskService) {}
 
-  public ngOnInit(): void {}
+  public async ngOnInit(): Promise<void> {
+    //such like a promise
+    /*     this.tasksService.getTasks().subscribe((tasks) => {
+      tasks = this.tasks;
+    }); */
+
+    this.tasks = await this.tasksService.getTasks();
+  }
 }
