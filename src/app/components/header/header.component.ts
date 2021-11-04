176116e24a2b73e886d259ec2abee8bf8e25f6b2
Eliel <elielmichel@gmail.com>
2021-11-03 21:10:38 -0300

    chore: set up routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { UiService } from 'src/app/services/ui.service';
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   public showAddTask: boolean;
   public subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddTask = value));
@@ -23,5 +24,9 @@ export class HeaderComponent implements OnInit {
 
   public handleAddTask(): void {
     this.uiService.toggleAddTask();
+  }
+
+  public hasRoute(route: string): boolean {
+    return this.router.url === route;
   }
 }
